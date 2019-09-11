f1b3d2a6079e67ccf1a996d31bdc88aa40ed0965
Farrell Nathaniel <farrellnathaniel25@gmail.com>
2019-09-11 21:46:46 +0700

    Update MainActivity.kt
    deleted some stray quotes

--- Meeting03 - LoremGenerator/app/src/main/java/com/github/frosthard/MainActivity.kt
+++ Meeting03 - LoremGenerator/app/src/main/java/com/github/frosthard/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         "Space Ipsum",
         "For those who have seen the Earth from space, and for the hundreds and perhaps thousands more who will, the experience most certainly changes your perspective. The things that we share in our world are far more valuable than those which divide us.\n" +
                 "It suddenly struck me that that tiny pea, pretty and blue, was the Earth. I put up my thumb and shut one eye, and my thumb blotted out the planet Earth. I didn’t feel like a giant. I felt very, very small.\n" +
-                "Science has not yet mastered prophecy. We predict too much for the next year and yet far too little for the next 10.\n" + "" +
+                "Science has not yet mastered prophecy. We predict too much for the next year and yet far too little for the next 10.\n" +
                 "We choose to go to the moon in this decade and do the other things, not because they are easy, but because they are hard, because that goal will serve to organize and measure the best of our energies and skills, because that challenge is one that we are willing to accept, one we are unwilling to postpone, and one which we intend to win.\n" +
                 "There can be no thought of finishing for ‘aiming for the stars.’ Both figuratively and literally, it is a task to occupy the generations. And no matter how much progress one makes, there is always the thrill of just beginning."
     )
